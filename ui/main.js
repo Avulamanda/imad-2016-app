e085ef3783a6e27f5922d50653e76b53dd89fab7
Avulamanda <avamsimohan19@outlook.com>
2016-09-23 15:36:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 // move the image
 var img = document.getElementById('madi');
-madi.onclick = function () {
+img.onclick = function () {
     img.style.marginleft = '200px';
 };
